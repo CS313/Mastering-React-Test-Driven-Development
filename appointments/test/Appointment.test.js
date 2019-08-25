c5fa2396f71a320de674fb5c0231ee2c74e3b927
CS313 <krinasav@gmail.com>
2019-08-25 22:04:02 +0300

    [refactor] - Refactoring test code

--- appointments/test/Appointment.test.js
+++ appointments/test/Appointment.test.js
@@ -2,17 +2,24 @@ import React from "react";
 import ReactDOM from "react-dom";
 import { Appointment } from "../src/Appointment";
 
+let customer;
+let container;
+
+beforeEach(() => {
+  container = document.createElement("div");
+});
+
+const render = component => ReactDOM.render(component, container);
+
 describe("Appointment", () => {
   it("renders the customer first name", () => {
-    const customer = { firstName: "Ashley" };
-    const container = document.createElement("div");
-    ReactDOM.render(<Appointment customer={customer} />, container);
+    customer = { firstName: "Ashley" };
+    render(<Appointment customer={customer} />);
     expect(container.textContent).toMatch("Ashley");
   });
   it("renders another customer first name", () => {
-    const customer = { firstName: "Jordan" };
-    const container = document.createElement("div");
-    ReactDOM.render(<Appointment customer={customer} />, container);
+    customer = { firstName: "Jordan" };
+    render(<Appointment customer={customer} />);
     expect(container.textContent).toMatch("Jordan");
   });
 });
